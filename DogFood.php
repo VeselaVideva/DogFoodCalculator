fbf7056d5a51b76fa72ca94d79e1f5969f97feea
Vesela Videva <33596898+VeselaVideva@users.noreply.github.com>
2020-10-15 10:46:48 +0300

    Update DogFood.php

--- DogFood.php
+++ DogFood.php
@@ -1,21 +1,17 @@
 <?php
 
-$weight = readline();
-$age = readline();
-$coef_1 = 2;
-$coef_2 = 1;
+$weight = readline();   // kilograms
+$age = readline();      // months
+$puppy = 2;
+$adult = 1;
 
 if ($age <= 9) {
     echo "Your dog needs ";
-    echo (70 * $weight) * 0.75 * $coef_1;
+    echo (70 * $weight) * 0.75 * $puppy;
     echo " kcal on a daily basis.";
 }
 if ($age > 12)  {
     echo "Your dog needs ";
-    echo (70 * $weight) * 0.75 * $coef_2;
+    echo (70 * $weight) * 0.75 * $adult;
     echo " kcal on a daily basis.";
 }
-
-
-
-
